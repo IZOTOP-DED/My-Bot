063b0e5ba5a4907c99ef27d82a9eb15d43893af6
IZOTOP-DED <acodrev@gmail.com>
2021-03-07 20:24:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,12 +18,12 @@ bot.on('message', (message) => {
     if(['641019353831178240', '816220439584047104', '816220705796128799', '816220863409029151', '817160538294321232', '817368521653616671'].includes(message.channel.id)) {
         [{id: '641019353831178240', lang: 'ru'}, {id: '816220439584047104', lang: 'fr'}, {id: '816220705796128799', lang: 'it'}, {id: '816220863409029151', lang: 'de'}, {id: '817160538294321232', lang: 'en'}, {id: '817368521653616671', lang: 'es'}].forEach(i => {
             if(i.id == message.channel.id) return;
-            translate(message.content, {from: translateMap[message.channel.id], to: i.lang}).then(res => {
+            translate(message.content ?? '', {from: translateMap[message.channel.id], to: i.lang}).then(res => {
                 bot.channels.cache.get(i.id).fetchWebhooks().then((item) => {
                     item.random().send(res.text ?? '', {
-                        username: message.member.nickname ?? message.author.username,
-                        avatarURL: message.author.displayAvatarURL(),
-                        ...(!!message.attachments && {files: message.attachments.array().map(att => ({attachment: att.url, name: att.name}))})
+                            username: message.member.nickname ?? message.author.username,
+                            avatarURL: message.author.displayAvatarURL(),
+                            ...(!!message.attachments && {files: message.attachments.array().map(att => ({attachment: att.url, name: att.name}))})
                     })
                 })        
             })
